f6b78555e5eb905d507ea024bda926bff7a0ffaa
Max <bespalov.m.9@ya.ru>
2023-01-03 09:56:46 +0300

    AppTest was populated with tests

--- src/test/java/besmax/app/AppTest.java
+++ src/test/java/besmax/app/AppTest.java
@@ -1,6 +1,8 @@
 package besmax.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyChar;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -46,4 +48,46 @@ public class AppTest {
         verify(consoleForTest, times(1)).printFromFile("output_my_turn");
     }
 
+    @Test
+    void makeUserMoveTakesNumber() {
+        Mockito.when(playgroundForTest.occupyPositionOnTheField(3, 'X')).thenReturn(true);
+        doNothing().when(consoleForTest).printFromFile(anyString());
+
+        assertTrue(appForTest.makeUserMove(3));
+        verify(consoleForTest, times(1)).printFromFile("output_move_ok");
+    }
+
+    @Test
+    void makeUserMoveDoesNotTakeNumber() {
+        Mockito.when(playgroundForTest.occupyPositionOnTheField(3, 'X')).thenReturn(false);
+        doNothing().when(consoleForTest).printFromFile(anyString());
+
+        assertFalse(appForTest.makeUserMove(3));
+        verify(consoleForTest, times(1)).printFromFile("input_wrong_cell");
+    }
+
+    @Test
+    void prepareFieldCallsMethods() {
+        doNothing().when(playgroundForTest).setSize(anyInt());
+        doNothing().when(playgroundForTest).prepareFieldForGame();
+        doNothing().when(consoleForTest).print(anyString());
+        Mockito.when(playgroundForTest.showCurrentPlayground()).thenReturn("xxx");
+
+        appForTest.prepareField(3);
+
+        verify(playgroundForTest, times(1)).setSize(anyInt());
+        verify(playgroundForTest, times(1)).prepareFieldForGame();
+        verify(consoleForTest, times(1)).print(anyString());
+
+    }
+
+    @Test
+    void setSizeOfFieldDoesNotTakeIncorrectNumber() {
+        doNothing().when(consoleForTest).printFromFile(anyString());
+        Mockito.when(consoleForTest.readNumber()).thenReturn(2).thenReturn(5);
+
+        appForTest.setSizeOfField();
+
+        verify(consoleForTest, times(2)).printFromFile("input_field");
+    }
 }
